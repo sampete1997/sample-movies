858741d3141bea05682ff129f01e34075f742fc7
Sohel Patel <68470296+sampete1997@users.noreply.github.com>
2023-05-13 05:33:47 +0000

    react odespace testing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [close, setClose] = useState(false)
+  const handleClose = () => {
+    setClose(true)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <p onClick={() => setClose(false)}>ON</p>
+      <div className='modal' style={close ? { display: 'none' } : {}}>
+        <div>      React Interview Questions for Freshers
+          1. What is React?
+          React is a front-end and open-source JavaScript library which is useful in developing user interfaces specifically for applications with a single page. It is helpful in building complex and reusable user interface(UI) components of mobile and web applications as it follows the component-based approach.
+        </div>
+        <input type='text'></input>
+        <div onClick={handleClose}>X</div>
+      </div>
     </div>
   );
 }
